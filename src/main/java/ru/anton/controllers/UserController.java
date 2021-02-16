dfaf687bd8397e7e94ea4fcf8ca2b2415b9b9ad1
Anton Zhiliakov <gofranich@gmail.com>
2021-02-16 22:16:27 +0600

    Create admin panel

---
+++ src/main/java/ru/anton/controllers/UserController.java
@@ -0,0 +1,34 @@
+package ru.anton.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.anton.models.Role;
+import ru.anton.models.User;
+import ru.anton.repo.UserRepo;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserRepo userRepo;
+
+    public UserController(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @GetMapping
+    public String userList(Model model) {
+        model.addAttribute("users", userRepo.findAll());
+        return "userList";
+    }
+
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Model model){
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "userEdit";
+    }
+}
